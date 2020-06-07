bb2b47dd6290533b82643a51c5ffcdd45b94fdf3
Pascal Sommer <p@pascalsommer.ch>
2020-06-07 05:13:13 +0200

    task 4 use augmentation: overfit

--- task4/task4_pascal_io.py
+++ task4/task4_pascal_io.py
@@ -8,21 +8,30 @@ nr_images = 10000
 
 def get_triplets(filename):
 	path = data_path + filename
-	data = pd.read_csv(path, sep=' ', header=None).to_numpy()
+	# we can't use to_numpy because piz daint only has pandas 0.23.3
+	data = np.array(pd.read_csv(path, sep=' ', header=None).values)
 	assert(data.ndim == 2)
 	assert(data.shape[1] == 3)
 	return data
 
 def get_image(img_path):
 	img = image.load_img(img_path, target_size=(224, 224))
-	x = image.img_to_array(img)
-	x = np.expand_dims(x, axis=0)
-	x = preprocess_input(x)
-	return x
+	return image.img_to_array(img)
 
 def get_image_nr(nr):
 	img_path = data_path + "food/{:05d}.jpg".format(nr)
 	return get_image(img_path)
 
+def flip(img):
+	return img[:,::-1]
+
+def preprocess_image(img):
+	x = np.expand_dims(img, axis=0)
+	x = preprocess_input(x)
+	return x
+
 def get_all_images():
-	return np.vstack([get_image_nr(idx) for idx in range(nr_images)])
+	return np.vstack([preprocess_image(get_image_nr(idx)) for idx in range(nr_images)])
+
+def get_all_images_flipped():
+	return np.vstack([preprocess_image(flip(get_image_nr(idx))) for idx in range(nr_images)])
--- task4/task4_pascal_main.py
+++ task4/task4_pascal_main.py
@@ -8,7 +8,7 @@ import numpy as np
 
 import task4_pascal_io as io4
 
-def construct_preprocessing_model():
+def construct_imagenet_model():
 	pretrained_model = ResNet50(weights='imagenet')
 	inner_layer = pretrained_model.get_layer(name='avg_pool')
 	return Model(
@@ -40,7 +40,8 @@ def construct_distance_layer(name):
 
 def construct_branch_layers(input_shape):
 	inp = keras.Input(shape=input_shape)
-	branch_inner = keras.layers.Dense(32, name='branch_inner', activation='relu')(inp)
+	branch_inner = keras.layers.Dense(64, name='branch_inner_1', activation='relu')(inp)
+	branch_inner = keras.layers.Dense(32, name='branch_inner_2', activation='relu')(branch_inner)
 	branch_output = keras.layers.Dense(16, name='branch_out')(branch_inner)
 	return Model(
 		inputs = inp,
@@ -50,7 +51,7 @@ def construct_branch_layers(input_shape):
 
 
 def construct_siamese_model():
-	input_shape = (2048,) # preprocessing_model.output.shape
+	input_shape = (2048,)
 
 	input_a = keras.Input(shape=input_shape, name='input_a')
 	input_b = keras.Input(shape=input_shape, name='input_b')
@@ -58,7 +59,7 @@ def construct_siamese_model():
 
 	# the weights are shared in all three branches
 	branch = construct_branch_layers(input_shape)
-	keras.utils.plot_model(branch, "siamese_branch.png", show_shapes=True)
+	# keras.utils.plot_model(branch, "siamese_branch.png", show_shapes=True)
 	out_a = branch(input_a)
 	out_b = branch(input_b)
 	out_c = branch(input_c)
@@ -72,68 +73,114 @@ def construct_siamese_model():
 		name="distance_comparison"
 	)([dist_ab, dist_ac])
 
-	return Model(
+	model = Model(
 		inputs = [input_a, input_b, input_c],
 		outputs = comparison
 	)
+	# keras.utils.plot_model(model, "model.png", show_shapes=True)
+	return model
 
-def assemble_triplets(data, triplets):
-	a = np.array([data[t[0]] for t in triplets])
-	b = np.array([data[t[1]] for t in triplets])
-	c = np.array([data[t[2]] for t in triplets])
-	return [a,b,c]
+def assemble_triplets(data, data_flipped, triplets, use_flipped=False):
+	d = data
+	df = data_flipped
 
-# images = io4.get_all_images()
-# preprocessing_model = construct_preprocessing_model()
-# imagenet_representations = preprocessing_model.predict(images)
-# np.save('latent.npy', imagenet_representations)
+	# This code looks way scarier than it should...
+	# combinations is all 8 elements of the cartesian product of three bools, it's used
+	# to select whether to use a normal or a flipped image for A,B,C.
+	# After that we just return those 8 rows per triplet
 
-print("\n\nload data")
-imagenet_representations = np.load('latent.npy')
-ir = imagenet_representations
+	combinations = [[0,0,0]]
+	if use_flipped:
+		combinations = [[a,b,c] for a in [0,1] for b in [0,1] for c in [0,1]]
 
-test_triplets = io4.get_triplets('test_triplets.txt')
-train_triplets = io4.get_triplets('train_triplets.txt')
+	return [np.array([[d,df][c[i]][t[i]] for t in triplets for c in combinations]) for i in [0,1,2]]
 
-print("\n\nconstruct model")
-model = construct_siamese_model()
-keras.utils.plot_model(model, "model.png", show_shapes=True)
-model.summary()
 
-# model.compile(
-#     optimizer=keras.optimizers.Adam(1e-3),
-#     loss=keras.losses.MeanAbsoluteError(),
-# )
+def apply_imagenet_to_dataset():
+	imagenet_model = construct_imagenet_model()
 
-# print("\n\nassemble triplets")
-# x = assemble_triplets(imagenet_representations, train_triplets)
-# y = np.zeros(shape=(train_triplets.shape[0], 1))
-# assert(len(x) == 3)
-# assert(x[0].shape[0] == y.shape[0])
+	images = io4.get_all_images()
+	out = imagenet_model.predict(images)
+	np.save('imagenet.npy', out)
 
-# print("\n\nfit model")
-# model.fit(
-#     x=x,
-#     y=y,
-#     batch_size=32,
-#     epochs=3,
-#     verbose=1,
-#     callbacks=None,
-#     validation_split=0.1,
-#     shuffle=True,
-# )
+	images_flipped = io4.get_all_images_flipped()
+	out_flipped = imagenet_model.predict(images_flipped)
+	np.save('imagenet_flipped.npy', out_flipped)
 
-# dist_model = Model(
-# 	inputs = model.input,
-# 	outputs = [model.get_layer('dist_ab').output, model.get_layer('dist_ac').output]
-# )
+def train_model(imagenet_rep, imagenet_rep_flipped, train_triplets):
+	model = construct_siamese_model()
+	model.compile(
+		optimizer=keras.optimizers.Adam(1e-3),
+		loss=keras.losses.MeanAbsoluteError(),
+	)
 
-# print("\n\nevaluate model")
-# [ab, ac] = dist_model(assemble_triplets(imagenet_representations, test_triplets))
+	x = assemble_triplets(imagenet_rep, imagenet_rep_flipped, train_triplets, use_flipped=True)
+	y = np.zeros(shape=(x[0].shape[0], 1))
 
-# Task output specification
-# 0 if the dish in A is closer in taste to C than to B
-# 1 if the dish in A is closer in taste to B than to C
+	# It can take a while to train the model, so we save the weights and can load
+	# them again later.
+	load_saved_weights = True
 
-# output = (np.sign(ac-ab) + 1) / 2
-# np.savetxt('out.csv', output.astype(int), fmt='%i')
+	if load_saved_weights:
+		saved_weights = [np.load('weights/w{}.npy'.format(i)) for i in range(6)]
+		model.set_weights(saved_weights)
+	else:
+		model.fit(
+			x=x,
+			y=y,
+			batch_size=32,
+			epochs=6,
+			verbose=1,
+			callbacks=None,
+			validation_split=0.1,
+			shuffle=True,
+		)
+
+		w = model.get_weights()
+		[np.save('weights/w{}'.format(i),w[i]) for i in range(6)]
+
+	return model
+
+
+def main():
+	imagenet_rep = np.load('imagenet.npy')
+	imagenet_rep_flipped = np.load('imagenet_flipped.npy')
+
+	train_triplets = io4.get_triplets('train_triplets.txt')
+	test_triplets = io4.get_triplets('test_triplets.txt')
+
+	model = train_model(imagenet_rep, imagenet_rep_flipped, train_triplets)
+
+	dist_model = Model(
+		inputs = model.input,
+		outputs = [model.get_layer('dist_ab').output, model.get_layer('dist_ac').output]
+	)
+
+	[ab, ac] = dist_model(
+		assemble_triplets(imagenet_rep, imagenet_rep_flipped, test_triplets, use_flipped=False)
+	)
+
+	# Task output specification
+	# 0 if the dish in A is closer in taste to C than to B
+	# 1 if the dish in A is closer in taste to B than to C
+
+	output = (np.sign(ac-ab) + 1) / 2
+	np.savetxt('out.csv', output.astype(int), fmt='%i')
+
+	print("\n\ndone")
+
+
+
+
+################################################################################
+################################################################################
+#######
+#######   change stuff here:
+#######
+
+# Uncommet this function to generate the files imagenet.npy and imagenet_flipped.npy
+apply_imagenet_to_dataset()
+
+# Uncomment this function once the files imagenet.npy and imagenet_flipped.npy are created
+# to train and evaluate the model
+main()
